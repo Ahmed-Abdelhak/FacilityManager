9270e00448c05532ab21bc5fc97db86dd92c9d13
EngineerAhmedMo <w-rl@hotmail.com>
2019-06-13 22:43:39 +0200

    Implementing the Dashboard

--- BuildingFacilityManager/Areas/Admin/Controllers/DashboardController.cs
+++ BuildingFacilityManager/Areas/Admin/Controllers/DashboardController.cs
@@ -1,19 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using BuildingFacilityManager.Controllers;
+using BuildingFacilityManager.Models;
+using BuildingFacilityManager.ViewModels;
 
 namespace BuildingFacilityManager.Areas.Admin.Controllers
 {
     public class DashboardController : AdminAuthorizationController
     {
-        public ActionResult Index()
+        private readonly ApplicationDbContext _context;
+
+
+        public DashboardController()
         {
-            return View();
+            _context = new ApplicationDbContext();
         }
 
-        
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+            _context.Dispose();
+        }
+
+
+        public ActionResult Index()
+        {
+            var model = new DashBoardViewModel()
+            {
+                WorkOrders = _context.WorkOrders.Where(w => w.Date >= DateTime.Today)
+                   .Include(w => w.Asset)
+                   .Include(w => w.Asset.Space)
+                   .Include(w => w.Asset.Space.Storey).ToList(),
+
+                Assets = _context.Assets.Where(a => a.InstallationDate >= DateTime.Today)
+                    .Include(a => a.Space)
+                    .Include(a => a.RelatedAssets)
+                    .ToList()
+            };
+            return View(model);
+        }
+
+
     }
 }
---
+++ BuildingFacilityManager/ViewModels/DashBoardViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using BuildingFacilityManager.Models.Assets;
+using BuildingFacilityManager.Models.Work_Order;
+
+namespace BuildingFacilityManager.ViewModels
+{
+    public class DashBoardViewModel
+    {
+        public ICollection<WorkOrder> WorkOrders { get; set; }
+        public ICollection<Asset> Assets { get; set; }  
+    }
+}
